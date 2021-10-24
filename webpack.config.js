cc9c07636e0a3bd92d0ae067a390db3ce1a3d797
unknown <calwong42991@gmail.com>
2021-10-24 12:05:59 -0700

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: path.join(_dirname, '/dist'),
+        filename: 'index.bundle.js'
+
+    },
+    devServer: {
+        port: 3010,
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
